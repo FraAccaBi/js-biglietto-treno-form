27691122a41562cd2465d93379e444d16372c50e
FraAccaBi <francescahuda@gmail.com>
2022-03-26 11:28:00 +0100

    complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,39 +5,52 @@ let nameValue;
 let kmValue;
 let ageValue; 
 let prezzo;
-let elementOfferta = document.getElementById('offerta');
-let elementCostoBiglietto = document.getElementById('costo_biglietto');
 
-const bottoneGenera = document.getElementById('bottone')
 
-const nomePasseggero = document.getElementById('nome_passeggero')
 
-const elementSconto = document.getElementById('offerta')
+
+const nomePasseggero = document.getElementById('nome_passeggero');
+const elementOfferta = document.getElementById('offerta');
+const numeroCarrozza = document.getElementById('carrozza');
+const codiceBiglietto = document.getElementById('codice');
+const elementCostoBiglietto = document.getElementById('costo_biglietto');
+
+
+
+const bottoneGenera = document.getElementById('bottone');
+
+
+
+
 
 //aggiungo l'elementListener che si attiva al click del bottone Genera
+
 bottoneGenera.addEventListener ('click', function() {
     nameValue = document.getElementById("user_name").value;
     kmValue = Number(document.getElementById("km").value); //converto in nr il dato ottenuto come input
     prezzo = kmValue * (0.21) //formula per il costo a km
-    ageValue = document.getElementById("user_age").value; //ottengo le lezioni per il cilo che segue
+    ageValue = Number(document.getElementById("user_age").value); //ottengo la fascia d'età
     console.log(nameValue);
     console.log(kmValue);
     console.log(ageValue);
 
     //attribuisco gli sconti alla fascia d'età selezionata
-    if ( ageValue = '2') {
+    if ( ageValue == 2) {
         var sconto = kmValue * (0.21) * (0.2)
         console.log('Sconto di euro ' + sconto );
         prezzo = prezzo - sconto 
         console.log('Il prezzo del biglietto è di euro ' + prezzo);
-    } else if (ageValue = '3') {
+        elementOfferta.innerHTML += 'Sconto Minorenne'
+    } else if (ageValue == 3) {
         var sconto = kmValue * (0.21) * (0.4)
         console.log('Sconto di euro ' + sconto);
         prezzo = prezzo - sconto 
         console.log('Il prezzo del biglietto è di euro ' + prezzo);
+        elementOfferta.innerHTML += 'Sconto Over 65'
     } else {
         console.log('nessuno sconto applicabile');
         console.log('Il prezzo del biglietto è di euro ' + prezzo);
+        elementOfferta.innerHTML += 'Nessuno sconto disponibile'
     }
     
     
@@ -49,16 +62,12 @@ bottoneGenera.addEventListener ('click', function() {
 
     nomePasseggero.innerHTML += nameValue
 
-    if (ageValue = '2'){
-        elementSconto.innerHTML += 'Sconto Minorenne'
-    } else if (ageValue = '3'){
-        elementSconto.innerHTML += 'Sconto Over 65'
-    } else {
-        elementSconto.innerHTML += 'Nessuno sconto disponibile'
-    }
     
+    let random_biglietto = Math.ceil(Math.random() * 100000)
+    let random_carrozza = Math.ceil(Math.random() * 10)
 
-
+    codiceBiglietto.innerHTML += random_biglietto
+    numeroCarrozza.innerHTML += random_carrozza
 })
 
 
